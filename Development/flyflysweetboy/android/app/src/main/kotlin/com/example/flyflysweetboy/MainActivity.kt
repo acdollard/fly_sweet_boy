6297e0effacadc4d71ed26a4758ae1a501bbd6f1
alexDollard <alexdollard141@gmail.com>
2021-02-23 13:38:57 -0600

    overhauled the app and set a nice blueGrey theme

---
+++ Development/flyflysweetboy/android/app/src/main/kotlin/com/example/flyflysweetboy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flyflysweetboy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Development/flyflysweetboy/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Fly Sweet Boy'),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        backgroundColor: Colors.blueGrey[400],
+      ),
+    ),
+  );
+}
